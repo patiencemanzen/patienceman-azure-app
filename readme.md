9e26547b557dcb35536cf9b6dababf7a33a2c7c6
Manirabona Patience <hseal419@gmail.com>
2024-03-26 14:11:43 +0200

    Update readme and add comment explaining cjs file extension

--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-# JavaScript on Azure Learn Path - Module 2 - Deploying a basic app to Azure
+# Node.js on Azure - Module 2 - Deploying a basic app to Azure
 
 This Learn module deploys a basic app to Azure App Service.
 
--- run.cjs
+++ run.cjs
@@ -1,1 +1,7 @@
+/**
+ * The cjs file extension is important because it tells Node that this file is not a ES module, 
+ * as it would expect because of "type": "module" in the package.json. It allows other CommonJS files 
+ * to include our new file - namely iisnode's interceptor.js. It again imports the server.js 
+ * which then runs fine as ES module.
+ */
 import("./server.js");
